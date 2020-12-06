8e8c240184515c1baff3be86a1cc7f86fe743eab
Albertus Andito <aandito7@gmail.com>
2020-12-06 19:22:14 +0000

    #17 find frequent relations (ran extractor on datasets)

---
+++ experiments/covid-dataset-relations.py
@@ -0,0 +1,23 @@
+# Script to get SPO triples from first dataset (from https://zenodo.org/record/4282522)
+
+from TripleExtractor import TripleExtractor
+import pprint
+import pandas as pd
+
+df = pd.read_csv('../../covid-dataset/cleaned-COVID Fake News Data.csv')
+
+def get_relations(triples):
+    relation = []
+    for sent in triples:
+        for triple in sent:
+            relation.append(triple['relation'])
+    return relation
+
+extractor = TripleExtractor(r'C:\Users\aandi\Documents\Uni\Final Year\FYP Code\stanford-corenlp-4.2.0')
+df['triples'] = [extractor.produce_triples(doc) for doc in df['headlines']]
+pprint.pprint(df['triples'])
+print(df['triples'].loc[1][0])
+print(df['triples'].loc[2][0])
+df['relations'] = df['triples'].apply(lambda x: get_relations(x))
+print(df['relations'])
+df.to_csv('cleaned-COVID Fake News Data-with-relations.csv', index=False)

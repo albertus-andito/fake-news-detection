b496ef0c1f7cd4107643142f282f2e041b891fdf
Albertus Andito <aandito7@gmail.com>
2020-12-06 19:21:14 +0000

    fixed TripleExtractor.py wrt json error handlings, print index for progress

--- TripleExtractor.py
+++ TripleExtractor.py
@@ -6,7 +6,6 @@ import pprint
 import requests
 import spacy
 
-
 class TripleExtractor:
     openie_props = {'annotators': 'openie', 'pipelineLanguage': 'en', 'outputFormat': 'json'}
     spotlight_url = 'https://api.dbpedia-spotlight.org/en/annotate?'
@@ -15,21 +14,24 @@ class TripleExtractor:
         self.coreNLP = StanfordCoreNLP(coreNlpPath)
         self.nlp = spacy.load('en')
         neuralcoref.add_to_pipe(self.nlp)
+        self.i = 0;
 
     def __del__(self):
         self.coreNLP.close()
 
     def produce_triples(self, document):
-        # TODO: coreference
+        # coreference
         document = self.coref_resolution(document)
 
         all_triples = self.extract_triples(document)
 
-        # TODO: map to dbpedia resource (dbpedia spotlight) for Named Entities
+        # map to dbpedia resource (dbpedia spotlight) for Named Entities
         all_triples = self.spot_entities_with_context(document, all_triples)
 
         # TODO: might still want to match subject/object to DBpedia, even if they're not really named entities?
         # TODO: extract relation???
+        self.i +=1
+        print(self.i)
         return all_triples
 
     def coref_resolution(self, document):
@@ -37,7 +39,11 @@ class TripleExtractor:
         return doc._.coref_resolved
 
     def extract_triples(self, document):
-        output_sentences = json.loads(self.coreNLP.annotate(document, self.openie_props))['sentences']
+        try:
+            output_sentences = json.loads(self.coreNLP.annotate(document, self.openie_props), encoding='utf-8')['sentences']
+        except json.decoder.JSONDecodeError as e:
+            print(e.msg)
+            return []
         all_triples = []
         for sentence in output_sentences:
             openie = sentence['openie']
@@ -50,7 +56,6 @@ class TripleExtractor:
                 }
                 triples.append(triple)
             all_triples.append(triples)
-        # pprint.pprint(all_triples)
         return all_triples
 
     def spot_entities(self, all_triples):
@@ -71,29 +76,28 @@ class TripleExtractor:
                                         params={'text': triple['object']},
                                         headers={'Accept': 'application/json'}).json()
                 print(triple['object'])
-                # pprint.pprint(response['Resources']) if 'Resources' in response else print("Wakwaw")
-                # print('----------------------------------------')
 
     def spot_entities_with_context(self, document, all_triples):
         # Do we need to split the sentences first or not? May help with context if not?
         sentences = tokenize.sent_tokenize(document)
         for sentence, triples in zip(sentences, all_triples):
-            response = requests.get(self.spotlight_url,
-                                params={'text': sentence},
-                                headers={'Accept': 'application/json'}).json()
-            # pprint.pprint(response['Resources']) if 'Resources' in response else None
+            try:
+                response = requests.get(self.spotlight_url,
+                                        params={'text': sentence},
+                                        headers={'Accept': 'application/json'}).json()
+            except json.decoder.JSONDecodeError as e:
+                print(e.msg)
+                response = None
+
             resources = response['Resources'] if 'Resources' in response else None
-            # pprint.pprint(resources)
 
-            for triple in triples:
-                for resource in resources:
-                    if triple['subject'] in resource['@surfaceForm']:
-                        triple['subject'] = resource['@URI']
-                    if triple['object'] in resource['@surfaceForm']:
-                        triple['object'] = resource['@URI']
-                # pprint.pprint(triple)
-
-            # print('-----------------------------')
+            if resources is not None:
+                for triple in triples:
+                    for resource in resources :
+                        if triple['subject'] in resource['@surfaceForm']:
+                            triple['subject'] = resource['@URI']
+                        if triple['object'] in resource['@surfaceForm']:
+                            triple['object'] = resource['@URI']
 
         return all_triples
 
@@ -103,7 +107,6 @@ if __name__ == "__main__":
     doc = "Barack Obama was born in Hawaii. He attended school in Jakarta."
     # doc = "Lionel Messi plays for Barcelona"
     # doc = "He was born in Washington"
-
     # triples = extractor.extract_triples(doc)
     # extractor.spot_entities(triples)
     # triples = extractor.spot_entities_with_context(doc, triples)

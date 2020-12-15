e49798eeddaff9e7d20cbcb2724f643491a9318a
Albertus Andito <aandito7@gmail.com>
2020-12-15 14:59:34 +0000

    #5 implemented scrapers for bbc, independent, and guardian

---
+++ knowledge-graph-updater/ArticleScraper.py
@@ -0,0 +1,73 @@
+import requests
+from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from requests_html import HTMLSession
+
+
+class ArticleScraper(ABC):
+
+    def __init__(self, db_connection):
+        self.db_client = MongoClient(db_connection)
+        self.db = self.db_client["fnd"]
+        self.db_collection = self.db["articles"]
+        self.db_collection.create_index("source", unique=True)
+
+    def execute(self, url):
+        self.save_to_db(self.scrape(url))
+
+    @abstractmethod
+    def scrape(self, url):
+        pass
+
+    def save_to_db(self, article):
+        self.db_collection.update_one({'source': article['source']}, {'$set': article}, upsert=True)
+
+
+class BbcScraper(ArticleScraper):
+    def scrape(self, url):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        text_elements = soup.find_all('div', {'data-component': 'text-block'})
+        texts = [elem.text for elem in text_elements]
+        return {
+            "headlines": [soup.find(id='main-heading').text],
+            "date": soup.time['datetime'],
+            "texts": texts,
+            "source": url
+        }
+
+
+class IndependentScraper(ArticleScraper):
+    def scrape(self, url):
+        session = HTMLSession()
+        page = session.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        header = soup.find(id='articleHeader')
+        content = soup.find(id='main')
+        text_elements = content.find_all('p')
+        texts = [elem.text for elem in text_elements]
+        return {
+            "headlines": [header.find('h1').text, header.find('h2').text],
+            "date": header.find('amp-timeago')['datetime'],
+            "texts": texts,
+            "source": url
+        }
+
+
+class GuardianScraper(ArticleScraper):
+    def scrape(self, url):
+        payload = {'api-key': '147a81bd-3ca0-4163-954f-a5982a016227', 'show-fields': 'headline,body,trailText'}
+        response = requests.get(url, params=payload).json()['response']['content']
+
+        soup = BeautifulSoup(response['fields']['body'], 'html.parser')
+        text_elements = soup.find_all('p')
+        texts = [elem.text for elem in text_elements]
+        return {
+            "headlines": [response['fields']['headline'], response['fields']['trailText']],
+            "date": response['webPublicationDate'],
+            "texts": texts,
+            "source": response['webUrl']
+        }
